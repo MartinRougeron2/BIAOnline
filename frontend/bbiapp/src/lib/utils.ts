18165f19decfc436e6979dcfc7505a68a16092c0
martinrougeron <martin.rougeron@epitech.eu>
2024-07-16 10:23:05 +0200

    fix utils

--- frontend/bbiapp/src/lib/utils.ts
+++ frontend/bbiapp/src/lib/utils.ts
@@ -1,12 +1,15 @@
-// from seconds to weeks, days, hours, minutes, seconds
 function numberSecToTime(sec: number) {
-    const weeks = Math.floor(sec / 604800);
-    const days = Math.floor((sec % 604800) / 86400);
-    const hours = Math.floor(((sec % 604800) % 86400) / 3600);
-    const minutes = Math.floor((((sec % 604800) % 86400) % 3600) / 60);
-    const seconds = (((sec % 604800) % 86400) % 3600) % 60;
+    const years = Math.floor(sec / 31536000);
+    const months = Math.floor((sec % 31536000) / 2592000);
+    const weeks = Math.floor(((sec % 31536000) % 2592000) / 604800);
+    const days = Math.floor(((sec % 31536000) % 2592000) / 86400);
+    const hours = Math.floor((((sec % 31536000) % 2592000) % 86400) / 3600);
+    const minutes = Math.floor(((((sec % 31536000) % 2592000) % 86400) % 3600) / 60);
+    const seconds = Math.floor((((((sec % 31536000) % 2592000) % 86400) % 3600) % 60));
 
     let time = '';
+    if (years > 0) time += years + 'y ';
+    if (months > 0) time += months + 'ms ';
     if (weeks > 0) time += weeks + 'w ';
     if (days > 0) time += days + 'd ';
     if (hours > 0) time += hours + 'h ';
